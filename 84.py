f6b9c98e4986a3dbb18212964beef350f64a25f4
Kayssu <k_alberto12@outlook.com>
2025-03-08 00:10:47 -0300

    Update 84.py

--- 84.py
+++ 84.py
@@ -4,20 +4,24 @@ confirmar = "S"
 cadastrados = 0
 pesados = []
 leves = []
+
 while confirmar != "N":
     nomepeso.append(str(input("Nome: ")))
     nomepeso.append(int(input("Peso: ")))
     pessoas.append(nomepeso[:])
+
     if cadastrados == 0:
         pesados.append(nomepeso[:])
         leves.append(nomepeso[:])
 
+    if cadastrados >= 1:
+        if pesados > pessoas and pesados > leves[0]:
+            pesados.clear()
+            pesados.append(nomepeso[:])
 
-    # if cadastrados >= 1:
-    #     if pesados[0][1] >= pessoas[0][1] :
-    #         pesados.append(nomepeso[:])
-    #     elif leves [0][1] < pessoas[cadastrados-1][1]:
-    #         leves.append(nomepeso[:])
+        elif leves < pessoas:
+            leves.clear()
+            leves.append(nomepeso[:])
 
 
     cadastrados += 1
