94cd0acba02316fca790bd0a12e6acbb200db785
Kayssu <k_alberto12@outlook.com>
2025-03-08 00:16:03 -0300

    Update 84.py

--- 84.py
+++ 84.py
@@ -5,20 +5,24 @@ cadastrados = 0
 pesados = []
 leves = []
 
+
 while confirmar != "N":
     nomepeso.append(str(input("Nome: ")))
     nomepeso.append(int(input("Peso: ")))
     pessoas.append(nomepeso[:])
 
+#Primeira rotação para os pesados e leves receberem valor
     if cadastrados == 0:
         pesados.append(nomepeso[:])
         leves.append(nomepeso[:])
 
+#Apartir da segunda rotação pra saber se são mais pesados ou leves e mudarem os valores anteriores
+    #Pesados:
     if cadastrados >= 1:
-        if pesados > pessoas and pesados > leves[0]:
+        if pesados > pessoas and pesados > leves:
             pesados.clear()
             pesados.append(nomepeso[:])
-
+    #Leves:
         elif leves < pessoas:
             leves.clear()
             leves.append(nomepeso[:])
@@ -27,6 +31,7 @@ while confirmar != "N":
     cadastrados += 1
     nomepeso.clear()
 
+#Rotação de confirmar caso o User digite algo diferente de S/N
     confirmar = input("Deseja continuar? [S/N] ").upper().strip()
     while confirmar != "S" and confirmar != "N":
         confirmar = input("Digite apenas: [S/N] ").upper().strip()
